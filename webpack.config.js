568a0f737ce56f124e1e1598ae0c7e0c8d8e87d8
Abdel <juniorperezpy@gmail.com>
2020-07-20 16:41:23 -0400

    Add webpack-dev-server package

---
+++ webpack.config.js
@@ -0,0 +1,36 @@
+const path = require('path');
+
+module.exports = {
+  mode: 'development',
+  entry: './public/src/index.js',
+  output: {
+    filename: 'main.js',
+    path: path.resolve(__dirname, 'public/dist'),
+  },
+  // module: {
+  //   rules: [
+  //     {
+  //       test: /\.css$/,
+  //       use: [
+  //         'style-loader',
+  //         'css-loader',
+  //       ],
+  //     },
+  //     {
+  //       test: /\.(png|svg|jpg|gif)$/,
+  //       use: [
+  //         'file-loader',
+  //       ],
+  //     },
+  //     {
+  //       test: /\.(woff|woff2|eot|ttf|otf)$/,
+  //       use: [
+  //         'file-loader',
+  //       ],
+  //     },
+  //   ],
+  // },
+  devServer: {
+    contentBase: './public/dist',
+  },
+};
