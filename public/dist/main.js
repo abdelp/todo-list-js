d06ee9539673a27632230d1d307d6996bf9d21d9
elbie-em <emoonga1993@gmail.com>
2020-07-21 19:08:10 +0200

    modify Project factory function internal methods

--- public/dist/main.js
+++ public/dist/main.js
@@ -93,7 +93,7 @@
 /*! no static exports found */
 /***/ (function(module, exports) {
 
-eval("\nconst Todo = (todo) => {\n  const updateTitle = (value) => {\n    todo.title = value;\n  }\n\n  const updateDesc = (value) => {\n    todo.description = value;\n  }\n\n  const updateDueDate = (value) => {\n    todo.dueDate = value;\n  }\n\n  const updatePriority = (value) => {\n    todo.priority\n  }\n  return {...todo\n         };\n}\n\nconst Project = () => {\n\n} \n\n\n\n//# sourceURL=webpack:///./public/src/index.js?");
+eval("\nconst Project = (title) => {\n  let toDoList = [];\n  \n  const addTodo = (todo) => {\n  \ttoDoList.push(todo);\n  }\n\n  const deleteTodo = (todo) => {\n    toDoList.splice(toDoList.indexOf(todo),1)\n  }\n\n  const readList = () => {\n    return toDoList;\n  }\n\t\n  return {title,addTodo,readList,deleteTodo}\n} \n\nconst Todo = (id,title,desc,dueDate,priority) => {\n\treturn {id,title,desc,dueDate,priority}\n}\n\nlet testp = Project('Default');\n\nconst t1 = Todo(1,'test1','This is a test',\"20-07-21\",'high');\nconst t2 = Todo(2,'test2','This is a test',\"20-07-21\",'high');\nconst t3 = Todo(3,'test3','This is a test',\"20-07-21\",'high');\n\ntestp.addTodo(t1);\ntestp.addTodo(t2);\ntestp.addTodo(t3);\n\nconst show = () => {\n  testp.readList().forEach(todo => {\n    console.log(todo);\n  });\n}\n\nshow();\n\ntestp.deleteTodo(t1);\nconsole.log(\"_______________________________\");\n\nshow();\n\n\n\n\n\n//# sourceURL=webpack:///./public/src/index.js?");
 
 /***/ })
 
--- public/src/index.js
+++ public/src/index.js
@@ -1,39 +1,48 @@
 
-const TodoCrud = (todo) => {
-  const updateTitle = (value) => {
-    todo.title = value;
-  }
-
-  const updateDesc = (value) => {
-    todo.description = value;
-  }
-
-  const updateDueDate = (value) => {
-    todo.dueDate = value;
-  }
-
-  const updatePriority = (value) => {
-    todo.priority
-  }
-  return {...todo
-         };
-}
-
-const Todo = (id,title,desc,dueDate,priority) => {
-	return {id,title,desc,dueDate,priority}
-}
-
-const Project = () => {
+const Project = (title) => {
   let toDoList = [];
   
   const addTodo = (todo) => {
-  	list.push(todo);
+  	toDoList.push(todo);
+  }
+
+  const deleteTodo = (todo) => {
+    toDoList.splice(toDoList.indexOf(todo),1)
   }
 
   const readList = () => {
     return toDoList;
   }
 	
-  return {title,addTodo}
+  return {title,addTodo,readList,deleteTodo}
 } 
+
+const Todo = (id,title,desc,dueDate,priority) => {
+	return {id,title,desc,dueDate,priority}
+}
+
+let testp = Project('Default');
+
+const t1 = Todo(1,'test1','This is a test',"20-07-21",'high');
+const t2 = Todo(2,'test2','This is a test',"20-07-21",'high');
+const t3 = Todo(3,'test3','This is a test',"20-07-21",'high');
+
+testp.addTodo(t1);
+testp.addTodo(t2);
+testp.addTodo(t3);
+
+const show = () => {
+  testp.readList().forEach(todo => {
+    console.log(todo);
+  });
+}
+
+show();
+
+testp.deleteTodo(t1);
+console.log("_______________________________");
+
+show();
+
+
 
