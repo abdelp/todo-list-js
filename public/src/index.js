6c26a0728664910b090905e8fddcea4533b2a9e8
Abdel <juniorperezpy@gmail.com>
2020-07-24 15:06:25 -0400

    Update location of todo and project forms inside modals

--- public/src/index.js
+++ public/src/index.js
@@ -1,7 +1,10 @@
 import * as Doman from './modules/doman';
 import * as Database from './modules/database';
 import * as Todo from './models/todo';
+
 import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/js/dist/util';
+import 'bootstrap/js/dist/modal';
 
 const addTodo = () => {
   const data = Doman.getFormValues('todo-form');
