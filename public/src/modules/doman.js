d9e8193603a191813b7d88e977225c79d4ce2a24
Abdel <juniorperezpy@gmail.com>
2020-07-27 18:53:53 -0400

    Add createList and addChild methods

--- public/src/modules/doman.js
+++ public/src/modules/doman.js
@@ -20,5 +20,25 @@ const hideModal = modalId => {
   $(modalId).modal('hide');
 }
 
-export {getFormValues,cleanForm, hideModal};
+const createList = (list) => {
+  let ul = document.createElement('ul');
+  ul.className = 'list-group';
+
+  list.forEach(item => {
+    let li = document.createElement('li');
+    li.className = 'list-group-item';
+    li.id = item.id;
+    li.innerText = item.title;
+    ul.appendChild(li);
+  });
+
+  return ul;
+};
+
+const addChild = (containerId, element) => {
+  let container = document.getElementById(containerId);
+  container.appendChild(element);
+};
+
+export {getFormValues,cleanForm, hideModal, createList, addChild};
 
