9b51bbe7241ad02e8785ac143e57a2c0e6a95211
Abdel <juniorperezpy@gmail.com>
2020-07-27 18:54:53 -0400

    Update format of getDoc and getDefaultProject

--- public/src/modules/database.js
+++ public/src/modules/database.js
@@ -17,20 +17,21 @@ const setCurrentProject = (projectId) => {
   localStorage.setItem('currentProject', projectId);
 };
 
-const getDoc = async (collection,params = []) => {
+const getDoc = async (collection, params = []) => {
   const collectionRef = firestore.collection(collection);
   if(params) {
     params.forEach(param => {
       collectionRef.where(param.key,param.sign,param.value);
-    })
+    });
   }
+
   let result = [];
 
   try {
     const docs = await collectionRef.get();
 
     docs.forEach(doc => {
-      result.push(doc.id);
+      result.push({id: doc.id, ...doc.data()});
     });
   } catch(error) {
     result = await error;
@@ -56,8 +57,9 @@ const getDefaultProject = async (userId) => {
   let result;
 
   try {
-    const params = [{key:"title",sign:"==",value:"default"},{key:"userId",sign:"==",value:userId}];
-    result = await getDoc('projects',params);
+    const params = [{key: "title", sign: "==", value: "default"},
+                    {key: "userId", sign: "==", value: userId}];
+    result = await getDoc('projects', params);
   } catch(error) {
     result = await error;
   }
