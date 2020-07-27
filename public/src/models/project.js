10160c715bb146d5829d1a4eee391d1aa0a0e7a0
Elbie Moonga <elbiemoonga@Elbies-MacBook-Pro.local>
2020-07-27 23:37:25 +0200

    change create to async and return project as result

--- public/src/models/project.js
+++ public/src/models/project.js
@@ -4,32 +4,22 @@ const params = ({title, description,userId}) => {
   return {title, description,userId};
 };
 
-const create = (data) => {
+const create = async (data) => {
   const collection = 'projects';
-  Database.add(collection, params(data))
-  .then(result => {
-    console.log(result);
-  })
-  .catch(error => {
-    console.log(error);
-  });
-};
-
-const getDefaultProject = async () => {
   let result;
-
   try {
-    result = await Database.getDoc('projects');
+    result = await Database.add(collection, params(data));
   } catch(error) {
     result = await error;
-  }
+  };
 
-  return result[0];
+  return result;
 };
+
 
 const allProjects = async () => {
   const collection = 'projects';
   const projects = Database.getDoc(collection);
 }
 
-export {create, getDefaultProject};
+export {create};
