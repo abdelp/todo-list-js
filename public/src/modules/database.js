f268fefe9bb0eef59c56b561a1bb2cb67460522e
Elbie Moonga <elbiemoonga@Elbies-MacBook-Pro.local>
2020-07-27 23:36:08 +0200

    add params to getDoc method

--- public/src/modules/database.js
+++ public/src/modules/database.js
@@ -17,12 +17,17 @@ const setCurrentProject = (projectId) => {
   localStorage.setItem('currentProject', projectId);
 };
 
-const getDoc = async (collection) => {
+const getDoc = async (collection,params = []) => {
   const collectionRef = firestore.collection(collection);
+  if(params) {
+    params.forEach(param => {
+      collectionRef.where(param.key,param.sign,param.value);
+    })
+  }
   let result = [];
 
   try {
-    const docs = await collectionRef.where("title", "==", "default").get();
+    const docs = await collectionRef.get();
 
     docs.forEach(doc => {
       result.push(doc.id);
@@ -43,10 +48,26 @@ const createUser = async () => {
   } catch(error) {
     result = await error;
   };
+
+  return result;
 };
+
+const getDefaultProject = async (userId) => {
+  let result;
+
+  try {
+    const params = [{key:"title",sign:"==",value:"default"},{key:"userId",sign:"==",value:userId}];
+    result = await getDoc('projects',params);
+  } catch(error) {
+    result = await error;
+  }
+
+  return result[0];
+};
+
 
 const getUserId = () => {
   return localStorage.getItem("userId");
 };
 
-export {add, getDoc, createUser, getUserId, setCurrentProject};
+export {add, getDoc, createUser, getUserId, setCurrentProject, getDefaultProject};
