5fe770cf8ce1f753bf0375a06959e7dec9ab82fb
Elbie-em <emoonga1993@gmail.com>
2020-07-29 21:58:15 +0200

    Add load projects method,Add pubsub module to enable reload of project lisr

--- public/src/index.js
+++ public/src/index.js
@@ -3,6 +3,7 @@ import * as Database from './modules/database';
 import * as Todo from './models/todo';
 import * as Project from './models/project';
 import * as User from './models/user';
+import PubSub from 'pubsub-js';
 
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/js/dist/util';
@@ -16,6 +17,7 @@ const addProject = () => {
   Project.create(data);
   Doman.cleanForm(formId);
   Doman.hideModal('project-modal');
+  PubSub.publish('LOAD PROJECTS');
 };
 
 let addProjectBtn = document.getElementById('add-project');
@@ -24,7 +26,7 @@ addProjectBtn.onclick = addProject;
 const addTodo = () => {
   const data = Doman.getFormValues('todo-form');
   const currentProject = Database.getCurrentProject();
-  Todo.create(currentProject,data);
+  Todo.create(currentProject, data);
   Doman.cleanForm('todo-form');
   Doman.hideModal('todo-modal');
 };
@@ -34,42 +36,49 @@ addTodoBtn.onclick = addTodo;
 
 const userId = Database.getUserId();
 
-if(!userId){
-  User.create({userName: 'test'})
-  .then(user => {
-    const data = {title: "Default", description: "This is the default project for your application", userId: user.id};
-    Project.create(data)
-    .then(project => {
-      Database.setCurrentProject(project.id);
-      Doman.setTitle(data.title);
+if (!userId) {
+  User.create({ userName: 'test' })
+    .then(user => {
+      const data = { title: "Default", description: "This is the default project for your application", userId: user.id };
+      Project.create(data)
+        .then(project => {
+          Database.setCurrentProject(project.id);
+          Doman.setTitle(data.title);
+        })
+        .catch(error => {
+          console.log(error);
+        });
+    });
+} else {
+  Database.getDoc('projects', { doc: Database.getCurrentProject() })
+    .then(doc => {
+      Doman.setTitle(doc[0].title);
     })
     .catch(error => {
       console.log(error);
     });
-  });
-}else {
-  Database.getDoc('projects', {doc: Database.getCurrentProject()})
-  .then(doc => {
-    Doman.setTitle(doc[0].title);
-  })
-  .catch(error => {
-    console.log(error);
-  });
 }
 
-Project.allProjects(userId)
-.then(result => {
-  console.log(result);
-  const onclickHandler = async function()
-  {
-    Database.setCurrentProject(this.id);
-    Doman.setTitle(this.innerHTML);
-    const todos = await Todo.allTodos(this.id);
-    const todoList = Doman.createList(todos, 'list-group-item', Doman.displayTodo);
-    Doman.cleanElement('todo-list');
-    Doman.addChild('todo-list', todoList);
-  };
 
-  const list = Doman.createList(result, 'btn btn-info m-2', onclickHandler);
-  Doman.addChild('projects-list', list);
-});
+const loadProjects = () => {
+  Doman.cleanElement('projects-list');
+  Project.allProjects(userId)
+    .then(result => {
+      console.log(result);
+      const onclickHandler = async function () {
+        Database.setCurrentProject(this.id);
+        Doman.setTitle(this.innerHTML);
+        const todos = await Todo.allTodos(this.id);
+        const todoList = Doman.createList(todos, 'list-group-item', Doman.displayTodo);
+        Doman.cleanElement('todo-list');
+        Doman.addChild('todo-list', todoList);
+      };
+
+      const list = Doman.createList(result, 'btn btn-info m-2', onclickHandler);
+      Doman.addChild('projects-list', list);
+    });
+}
+
+let token = PubSub.subscribe('LOAD PROJECTS', loadProjects);
+
+loadProjects();
