a59542139d7a15b349b96d26558c734bccd60cdf
Elbie-em <emoonga1993@gmail.com>
2020-08-03 22:09:52 +0200

    Clear logs

--- public/src/modules/auth.js
+++ public/src/modules/auth.js
@@ -4,6 +4,5 @@ const createUser = (email, password) => {
   firebase.auth().createUserWithEmailAndPassword(email, password).catch(error => {
     let errorCode = error.code;
     let errorMessage = error.message;
-    console.log(error);
   });
 }
